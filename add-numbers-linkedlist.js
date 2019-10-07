38da62ee4ba9e42ddfba631f94a2b567108d318e
wilandrade <wilandrade@Wils-MacBook-Pro.local>
2019-10-07 18:30:55 +0900

    Works, but can't handle very very big numbers.

--- add-numbers-linkedlist.js
+++ add-numbers-linkedlist.js
@@ -9,3 +9,45 @@ Output: 7 -> 0 -> 8
 Explanation: 342 + 465 = 807.
 */
 
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function (l1, l2) {
+  const getNumFromLinkedList = (node) => {
+    let curNode = node;
+    const nums = [];
+    while (curNode) {
+      nums.push(curNode.val);
+      curNode = curNode.next;
+    }
+    return Number(nums.reverse().join(''));
+  };
+
+  const numToLinkedList = (num) => {
+    const charArr = num.toString().split('');
+    let head = null;
+    let curNode = null;
+    for (let i = charArr.length - 1; i >= 0; i--) {
+      if (!head) {
+        head = new ListNode(charArr[i]);
+        curNode = head;
+      } else {
+        curNode.next = new ListNode(charArr[i]);
+        curNode = curNode.next;
+      }
+    }
+    return head;
+  };
+
+  const result = getNumFromLinkedList(l1) + getNumFromLinkedList(l2);
+  return numToLinkedList(result);
+};
