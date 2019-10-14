0f2aeaf58856e5d3b9276fc903646f0eb90577b7
Wil Andrade <andradewilker@gmail.com>
2019-10-15 00:28:04 +0900

    Update add-numbers-linkedlist.js

--- add-numbers-linkedlist.js
+++ add-numbers-linkedlist.js
@@ -21,18 +21,6 @@ Explanation: 342 + 465 = 807.
  * @param {ListNode} l2
  * @return {ListNode}
  */
-/**
- * Definition for singly-linked list.
- * function ListNode(val) {
- *     this.val = val;
- *     this.next = null;
- * }
- */
-/**
- * @param {ListNode} l1
- * @param {ListNode} l2
- * @return {ListNode}
- */
 var addTwoNumbers = function (l1, l2) {
   let a = l1;
   let b = l2;
